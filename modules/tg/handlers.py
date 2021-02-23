ad0c2a43d0b1092d480072388d8059d24d17001b
Sergey <iceman9831@gmail.com>
2021-02-23 18:59:49 +0300

    Nice welcome message strings

--- modules/tg/handlers.py
+++ modules/tg/handlers.py
@@ -1,5 +1,5 @@
 from aiogram import types
-from aiogram.utils.markdown import hcode, hbold
+from aiogram.utils.markdown import hcode, hbold, hlink
 
 from core.misc import dp, bot
 from modules.content_processor.aparser import parse_url
@@ -10,8 +10,9 @@ test_url = 'https://www.chanty.com/blog/components-effective-team-communication'
 @dp.message_handler(commands=['start'])
 async def cmd_start(m: types.Message):
     s = f"""
-Hi! Send me a link to get a small summary of an Article with hashtags. Should be useful for blogging or Linkedin.
+👋 Hi! Send me a link to get a small summary of an Article with hashtags. Should be useful for blogging or Linkedin.
 
+ℹ️ {hbold('Usage')}
 {hcode('<url> <max_summary_sentences> <max_summary_words> <language>')}
 
 url - mandatory article url
@@ -19,10 +20,12 @@ max_summary_sentences - optional, defaults to `{hcode('4')}`
 max_summary_words - optional, defaults to `{hcode('100')}`
 language - optional, defaults to `{hcode('en')}`
 
-{hbold('Example')}
+📝 {hbold('Example')}
 {test_url}
 
 {test_url} 5 100 en
+
+🤖 {hlink('Bot Source code', 'https://github.com/Kylmakalle/linkedin-bot')}
 """
     await m.reply(s, reply=False, disable_web_page_preview=True)
 
